ab62fef05e4764f97256b02cc954abfc57a128a1
ErikRyg <39767996+ErikRyg@users.noreply.github.com>
2018-06-27 15:18:38 +0200

    Add files via upload

--- turnAndMove/turnAndMove.ino
+++ turnAndMove/turnAndMove.ino
@@ -6,37 +6,45 @@ Stepper myStepper1(stepsPerRevolution, 10, 11);
 Stepper myStepper2(stepsPerRevolution, 8, 9);
 
 const float radiusFromWheel = 0.044;
-const float oneRevolution = 2*M_PI*0.044; //-> one revolution is 0.28 meters
+const float oneRevolution = 2 * M_PI * 0.044; //-> one revolution is 0.28 meters
 const float radiusFromRoboter = 0.0825;
 
 void setup() {
   // set the speed at 60 rpm:
   myStepper1.setSpeed(60);
   myStepper2.setSpeed(60);
-
-  // initialize the serial port:
   Serial.begin(9600);
-//  turn(180);
-  Move(300); //300mm
+
+  turn(307); //in degree
+  Move(685); //in mm
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-//  turn(180);
+
 }
 
-void turn(float degree){
-  float toCoveredDistance = (degree / radiusFromRoboter)*(1/3.0);
-  for(int i = 0; i < toCoveredDistance; i++){
-    myStepper1.step(1);
-    myStepper2.step(-1);
+//the degree is clockwise and turn the robot
+void turn(float degree) {
+  if (180 < degree) {
+    //turn the robot against clockwise
+    float toCoveredDistance = ((360 - degree) / radiusFromRoboter) * (1 / 2.81); //the factor 2.81 was determined metrologically by Erik
+    for (int i = 0; i < toCoveredDistance; i++) {
+      myStepper1.step(1);
+      myStepper2.step(1);
+    }
+  } else {
+    float toCoveredDistance = (degree / radiusFromRoboter) * (1 / 2.81); //the factor 2.81 was determined metrologically by Erik
+    for (int i = 0; i < toCoveredDistance; i++) {
+      myStepper1.step(-1);
+      myStepper2.step(-1);
+    }
   }
 }
 
-//distance in milli meters
-void Move(float distance){
-  for(int i = 0; i < (distance*3); i++){ //passt ungefähr mit mal drei
-    myStepper1.step(1);
+//distance in milli meters and moves the robot
+void Move(float distance) {
+  for (int i = 0; i < (distance * 2.9); i++) { //the factor 2.9 was determined metrologically by Erik
+    myStepper1.step(-1);
     myStepper2.step(1);
   }
 }
